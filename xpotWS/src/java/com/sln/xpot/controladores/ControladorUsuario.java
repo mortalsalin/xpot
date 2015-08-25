23c7ea0dad9a4e164982454e469ec8ea78d36b59
mortalsalin <rafael_salin@hotmail.com>
2015-08-25 16:15:53 -0300

    # Commit 8
    WebService

---
+++ xpotWS/src/java/com/sln/xpot/controladores/ControladorUsuario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sln.xpot.controladores;
+
+/**
+ *
+ * @author geral
+ */
+public class ControladorUsuario {
+    
+}
---
+++ xpotWS/src/java/com/sln/xpot/entidades/Usuario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sln.xpot.entidades;
+
+/**
+ *
+ * @author geral
+ */
+public class Usuario {
+    
+}
